3aaf1aef00ccc69b122285f52fa87fb41f08460a
mustak <mhyaqub@yahoo.com>
2024-03-28 14:56:53 +0000

    styled navigation

--- src/routes/navigation/navogation.component.jsx
+++ src/routes/navigation/navogation.component.jsx
@@ -1,12 +1,15 @@
 import { Outlet, Link } from 'react-router-dom';
 
+import Logo from '../../assets/logo.svg?react';
 import './navigation.styles.scss';
 
 const Navigation = () => {
     return (
         <>
             <div className="navigation">
-                <div className="logo-container">Logo</div>
+                <Link to="/" className="logo-container">
+                    <Logo className="logo" /> <span>Ameena&apos;s Fashion</span>
+                </Link>
                 <div className="nav-links-container">
                     <Link to="/" className="nav-link">
                         Home
