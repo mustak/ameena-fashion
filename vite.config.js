6e3152db5e65f5ad3180a49b354bdd57ed5941d3
mustak <mhyaqub@yahoo.com>
2024-04-26 22:44:44 +0100

    add path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+
+// eslint-disable-next-line no-undef
+const root = resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
     // base: '/ameena-fashion/',
+    resolve: {
+        alias: {
+            '@': resolve(root),
+        },
+    },
 });
